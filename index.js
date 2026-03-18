86295fb37246134d81a0af2055bf66e7f3204cce
AlanFalcon <dafalcon@gmail.com>
2026-03-18 13:49:37 -0700

    feat: wire latents processor into server entry point

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { createWebServer } = require('./src/interfaces/web-server');
 const { createGraveyardStore } = require('./src/storage/graveyard-store');
 const { createMemorialGenerator } = require('./src/api/memorial-generator');
 const { createTileGenerator } = require('./src/art/tile-generator');
+const { createLatentsProcessor } = require('./src/engine/latents-processor');
 const { createWriteQueue } = require('./src/storage/write-queue');
 const tileLibrary = require('./src/art/tile-library');
 const tileCompositor = require('./src/art/tile-compositor');
@@ -22,9 +23,11 @@ const graveyardStore = createGraveyardStore(path.join(__dirname, 'graveyard'));
 const writeQueue = createWriteQueue();
 const memorialGenerator = createMemorialGenerator(ANTHROPIC_API_KEY);
 const tileGenerator = createTileGenerator(ANTHROPIC_API_KEY, writeQueue, tileLibrary);
+const latentsProcessor = createLatentsProcessor(ANTHROPIC_API_KEY);
 
 const deps = {
   graveyardStore,
+  latentsProcessor,
   memorialGenerator,
   tileLibrary,
   tileCompositor,
